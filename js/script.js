f790fb54029b47537bf79090cedba41503b7626c
Fazilabi <fazilag@code.edu.az>
2019-12-15 09:15:16 +0400

    changed anything

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
 
 // 1ci variant
-// var bodyColor = document.body;
+var bodyColor = document.body;
+let hexCode=document.querySelector(".hexCode");
 
-// function setRandomColor() {
-//     $(bodyColor).css("background-color", getRandomColor());
-// }
+function setRandomColor() {
+    $(bodyColor).css("background-color", getRandomColor());
+}
 
-// function getRandomColor() {
-//     var letters = '0123456789ABCDEF';
-//     var color = '#';
-//     for (var i = 0; i < 6; i++) {
-//         color += letters[Math.floor(Math.random() * 16)];
-//     }
-//     return color;
-// }
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+        
+    }
+    hexCode.innerHTML=color;
+    return color;
+}
 
 
 // 2ci variant;
 
-let btn=document.querySelector(".colorBtn");
-let bodyBc=document.body;
-let colors=["yellow", "green", "red", "blue", "tomato"];
-btn.addEventListener("click", changeColor);
+// let btn=document.querySelector(".colorBtn");
+// let bodyBc=document.body;
+// let colors=["yellow", "green", "red", "blue", "tomato"];
+// btn.addEventListener("click", changeColor);
 
-function changeColor(){
-    let random=Math.floor(Math.random()*colors.length)
-    bodyBc.style.backgroundColor=colors[random];
+// function changeColor(){
+//     let random=Math.floor(Math.random()*colors.length)
+//     bodyBc.style.backgroundColor=colors[random];
 
-}
+// }
 
